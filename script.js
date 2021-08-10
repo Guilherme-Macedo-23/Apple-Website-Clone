d033bf4db2f19f83b16d3afe4beabd25c91d0680
Guilherme2303220 <79206432+Guilherme2303220@users.noreply.github.com>
2021-08-10 14:54:25 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+    $(".hamburguer").click(function () {
+      $(this).toggleClass("hamburguer-active")
+      $(".nav-list-mobile").toggleClass("active")
+    });
+  });
+  © 2021 GitHub, Inc.
